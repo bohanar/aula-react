573332a99750c9c5f434dae8b1099812448dc726
bohanar <bohanar@gmail.com>
2019-12-06 09:28:41 -0200

    lista de cadastro de pessoas

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Usuario from './Usuario';
+import './index.css';
+
+ReactDOM.render(
+  <Usuario />,
+  document.getElementById('root')
+);
